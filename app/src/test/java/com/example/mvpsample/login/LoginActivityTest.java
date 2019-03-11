81caa1abedb5dcb342c508030d25f5c12b57f7ea
Siraj <asrf@C02SG255G8WN.local>
2019-03-11 16:21:33 +0000

    Added test file

---
+++ app/src/test/java/com/example/mvpsample/login/LoginActivityTest.java
@@ -0,0 +1,17 @@
+package com.example.mvpsample.login;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginActivityTest {
+
+    @Test
+    public void shouldPass() {
+        Assert.assertEquals(1,2);
+    }
+
+
+
+}
